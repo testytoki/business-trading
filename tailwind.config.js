c6fb3814541df3c3495d73f857440422a3952854
Testy_Toky_13 <111314546+testytoki@users.noreply.github.com>
2023-12-12 03:41:33 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightOrange : 'hsl(22, 88%, 58%)',
+        brightOrangeLight: 'hsl(23, 95%, 52%)',
+        brightOrangeSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
